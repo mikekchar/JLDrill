bec7a9b4ee9c4bce95d9d29c892d122a32d6e5fe
Mike Charlton <mikekchar@gmail.com>
2007-12-11 15:06:02 +0900

    Added an rdoc task to the rake file

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,7 @@
 require 'rake'
 require 'spec/rake/spectask'
+require 'rake/rdoctask'
+
 
 files = FileList['lib/jldrill/*_spec.rb','lib/jldrill/Gtk/*_spec.rb']
 # This just makes sure that spec is looking in the right directories for
@@ -7,13 +9,15 @@ files = FileList['lib/jldrill/*_spec.rb','lib/jldrill/Gtk/*_spec.rb']
 ruby_opts = ["-I./lib/jldrill"]
 
 
-task :default => [:rcov]
+task :default => [:rcov, :rdoc]
 
+# Run the rspec tests
 Spec::Rake::SpecTask.new(:spec) do |t|
 	t.spec_files = files
 	t.ruby_opts = ruby_opts
 end
 
+# Run the rspec tests with the code coverage program rcov
 Spec::Rake::SpecTask.new(:rcov) do |t|
 	t.spec_files = files
 	t.rcov = true
@@ -22,9 +26,23 @@ Spec::Rake::SpecTask.new(:rcov) do |t|
 	t.ruby_opts = ruby_opts
 end
 
+# Run the rspec tests and if successful, run Heckle
 Spec::Rake::SpecTask.new(:heckle) do |t|
 	t.spec_files = files
-	t.spec_opts = ["--heckle Anjin"]
+	t.spec_opts = ["--heckle JLDrill"]
 	t.ruby_opts = ruby_opts
+end
+
+# Build the RDOC documentation tree.
+Rake::RDocTask.new(:rdoc) do |t|
+	t.rdoc_dir = 'doc'
+#  t.template = 'kilmer'
+#  t.template = 'css2'
+	t.title    = "JLDrill -- Japanese Language Drill program"
+	t.options << '--inline-source' <<
+		'--main' << 'README' <<
+		'--title' <<  'JLDrill -- Japanese Language Drill program' 
+	t.rdoc_files.include('README', 'COPYING', 'THANKS', 'data/jldrill/COPYING')
+	t.rdoc_files.include('lib/**/*.rb')
 end
 
