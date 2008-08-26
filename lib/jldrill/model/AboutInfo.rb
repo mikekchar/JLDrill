f3298f19a9bf9572801def2cc6b0e6ec2798f4da
Mike Charlton <mikekchar@gmail.com>
2008-08-26 11:17:01 +0900

    Modify the licence to be GPL 3.  Update the README to reflect the new release.

--- lib/jldrill/model/AboutInfo.rb
+++ lib/jldrill/model/AboutInfo.rb
@@ -8,18 +8,18 @@ module JLDrill
                     
         def initialize
             @authors = ["Mike Charlton"]
-		    @name = "GTK LDrill"
+		    @name = "JLDrill"
 		    @version = JLDrill::VERSION
 		    @copyright = "(C) 2005-2008 Mike Charlton"
-		    @comments = "Super Drill Program for Learning Japanese."
+		    @comments = "Drill Program for Learning Japanese."
             @website = "http://jldrill.rubyforge.org"
             @license = %Q[JLDrill - Drill Program for Learning Japanese
 Copyright (C) 2005-2008  Mike Charlton
 
-    This program is free software; you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any version specified by Mike Charlton.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,8 +27,7 @@ Copyright (C) 2005-2008  Mike Charlton
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]
         end
     end
