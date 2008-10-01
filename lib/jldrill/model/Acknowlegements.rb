1647ccf1d26ea305db5ffc952f76a6443b290509
Mike Charlton <mikekchar@gmail.com>
2008-10-01 13:55:58 +0900

    Added the KanjiStrokeOrders font to the distribution.  Added a word in the acknowlegements about it.  Updated the Install page in the website.  The Install page is almost complete.  It just needs a link to the actual package to install.  Also it might need to be altered if I make installation slightly simpler.

--- lib/jldrill/model/Acknowlegements.rb
+++ lib/jldrill/model/Acknowlegements.rb
@@ -2,44 +2,58 @@
 module JLDrill
     Acknowlegements = %Q[Acknowlegements
 
-    This program could not have been written if it were not for the
-    contribution that others have made.  Normally JLDrill will be distributed
-    with data files that are used for creating the drills.  These data
-    files required an enormous amount of work to compile and their authors
-    deserve to be given some credit.  
+This program could not have been written if it were not for the
+contribution that others have made.  Normally JLDrill will be
+distributed with data files that are used for creating the drills.
+These data files required an enormous amount of work to compile and
+their authors deserve to be given some credit.
 
-    In particular I want to thank The Electronic Dictionary Research
-    and Development Group at Monash University.  JLDrill uses the
-    EDICT files for it's reference dictionary.  Without this
-    monumental work, very little could be done in the world of free
-    software with respect to Japanese language training.  I am deeply
-    in their debt.
+In particular I want to thank The Electronic Dictionary Research and
+Development Group at Monash University.  JLDrill uses the EDICT files
+for it's reference dictionary.  Without this monumental work, very
+little could be done in the world of free software with respect to
+Japanese language training.  I am deeply in their debt.
 
-    I would also like to thank Thierry Bézecourt for the use of the
-    files which form the basis of the JLPT drills.  Thierry has very
-    kindly granted permission to use these files in this application
-    under the GPL license, thus saving me weeks of work.
+I would also like to thank Thierry Bézecourt for the use of the
+files which form the basis of the JLPT drills.  Thierry has very
+kindly granted permission to use these files in this application
+under the GPL license, thus saving me weeks of work.
 
-    Notes: 
+Finally, I would like to thank [RubyForge](http://rubyforge.org) for
+hosting this project.  It makes a big difference.
 
-      The EDICT files are the property of the Electronic
-      Dictionary Research and Development Group at Monash University,
-      and are used in conformance with the Group's licence.
+Notes 
+-----
+The EDICT files are the property of the Electronic Dictionary Research
+and Development Group at Monash University, and are used in
+conformance with the Group's licence.
 
-      Links: 
+Links: 
 
-        EDICT:   http://www.csse.monash.edu.au/~jwb/edict.html
-        Electronic Dictionary Research and Development Group: 
-                 http://www.csse.monash.edu.au/groups/edrdg/
-        License: http://www.csse.monash.edu.au/groups/edrdg/licence.html
+* [EDICT](http://www.csse.monash.edu.au/~jwb/edict.html)
+* [Electronic Dictionary Research and Development Group](http://www.csse.monash.edu.au/groups/edrdg/)
+* [License](http://www.csse.monash.edu.au/groups/edrdg/licence.html)
 
 
-      The JLPT files are Copyright (C) 2001 Thierry Bézecourt 
-      His website is http://www.thbz.org and he can be reached
-      at kanjimots@thbz.org
+The JLPT files are Copyright (C) 2001 Thierry Bézecourt 
 
-      More licence information can be found in the
-      data/jldrill/COPYING directory in the main distribution of this
-      application. 
+Please visit [Thierry's Website](http://www.thbz.org)
+
+The Grammar file was constructed using  Tae Kim's Japanese
+Guide to Japanese grammar.  That guide is licensed under a Creative
+Commons BY-NC-SA license (2.5).  It additionally includes vocabulary
+from EDICT.
+
+The stroke order information in the popup is provided by the
+KanjiStrokeOrder font:
+
+Copyright (C) 2004-2008 Ulrich Apel, the AAAA project and the Wadoku project
+All rights reserved.
+
+This font is distributed under a BSD license.
+
+More licence information can be found in the data/jldrill/COPYING
+directory in JLDrill's main distribution.
+ 
 ]
 end
