60e7a869b20fae480721a4402814daace55e73a7
Mike Charlton <mikekchar@gmail.com>
2008-12-14 16:13:04 +0900

    Fix test that failed because I removed edict.utf.  Changed it to edict.  Noticed that the new dictionary has 30000 new entries.

--- spec/jldrill/model/Edict/Edict_spec.rb
+++ spec/jldrill/model/Edict/Edict_spec.rb
@@ -28,18 +28,18 @@ module JLDrill
 	    
 	    it "should be able to read the file in chunks" do
 			dictDir = File.join(Config::DATA_DIR, "dict")
-            filename = File.join(dictDir, "edict.utf")
+            filename = File.join(dictDir, "edict")
             edict = Edict.new
             edict.file = filename
             edict.readLines
-            edict.lines.size.should be(114866)
+            edict.lines.size.should be(142339)
             edict.parseChunk(1000)
             edict.length.should be(1000)
             edict.index.should be(1000)
             edict.loaded.should be(false)
 # This part of the test is too slow to run every time right now
 #            while !edict.parseChunk(1000) do end
-#            edict.length.should be(114866)
+#            edict.length.should be(142339)
 	    end
 	
     end
