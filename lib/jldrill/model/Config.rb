1e30216928075cd79858b782d102168d96745221
Mike Charlton <mikekchar@gmail.com>
2009-10-23 16:14:55 +0900

    More TODO items.  Fix bug where getDataDir might return the wrong directory.  Change AppRun into a ruby script.  Now it can theoretically run out of it's own directory on Windows given that it can find ruby and all the supporting libraries.

--- lib/jldrill/model/Config.rb
+++ lib/jldrill/model/Config.rb
@@ -4,13 +4,25 @@ module JLDrill
     # Configuration data for JLDrill.  This is how JLDrill knows where
     # to find it's data.
     module Config
+        def Config::configSrcDir
+            File.expand_path(File.dirname(__FILE__))
+        end
+
+        # In a source repository, this gives the top level directory
+        # If the source has been installed by a package, then who knows
+        # where this is...
+        def Config::repositoryDir
+            File.expand_path(File.join(Config::configSrcDir, "../../.."))
+        end
+
         def Config::getDataDir
             if !Gem::datadir("jldrill").nil?
                 # Use the data directory in the Gem if it is available
                 File.expand_path(Gem::datadir("jldrill"))
             else
-                # Otherwise hope there is a data dir in current directory
-                File.expand_path('data/jldrill')
+                # Otherwise hope we are in a source repository and
+                # can find the data dir in the usual spot
+                File.join(repositoryDir, "data/jldrill")
             end
         end
     
