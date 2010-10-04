33792c2b4283d0cc1f5ea900d6c8a326c4500eff
Mike Charlton <mike@Wrook>
2010-10-04 15:40:21 +0900

    Refactoring LoadTanakaContext.

--- lib/jldrill/contexts/LoadTanakaContext.rb
+++ lib/jldrill/contexts/LoadTanakaContext.rb
@@ -1,18 +1,14 @@
 require 'Context/Context'
 require 'Context/Bridge'
 require 'jldrill/model/Config'
-require 'jldrill/views/ReferenceProgressView'
+require 'jldrill/contexts/FileProgressContext'
 
 module JLDrill
 
-	class LoadTanakaContext < Context::Context
-		
-	    attr_reader :filename, :reference
-        attr_writer :filename
+	class LoadTanakaContext < FileProgressContext
 		
 		def initialize(viewBridge)
 			super(viewBridge)
-            @tanaka = nil
 		end
 
         # Returns the filename (without the path) of the dictionary.
@@ -26,50 +22,12 @@ module JLDrill
         end
 
         # Returns the filename of the dictionary including the path
-        def dictionaryFilename
+        def getFilename
             return File.expand_path(dictionaryName, Config::TANAKA_DIR)
         end
-		
-		def createViews
-			@mainView = @viewBridge.ReferenceProgressView.new(self)
+
+        def getFile
+            return @parent.tanaka
         end
-        
-        def destroyViews
-            @mainView.destroy
-            @mainView = nil
-        end
-        
-        def readTanaka
-            if !@tanaka.nil?
-                @tanaka.file = dictionaryFilename
-                @tanaka.readLines
-                @mainView.idle_add do
-                    eof = @tanaka.parseChunk(100)
-                    @mainView.update(@tanaka.fraction)
-                    eof
-                end
-            end
-        end
-        
-		def enter(parent)
-			if (!parent.nil?)
-    			@tanaka = parent.tanaka
-    			if(!@tanaka.loaded? || 
-                   (@tanaka.file != dictionaryFilename))
-        			super(parent)
-        			readTanaka
-                    # The view will exit the context when the file
-                    # has finished loading.  This is because we
-                    # can't exit the context until the idle process
-                    # is removed, and this can only be done by the view.
-        		end
-            end
-		end
-		
-		def exit
-		    super
-			# Show the examples as soon as this has loaded
-			@parent.showExamplesContext.enter(@parent)
-		end
-    end
+    end		
 end
