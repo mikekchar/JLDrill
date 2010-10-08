b83d31db4fa1b29aaa078bac82e3b81320f01466
Mike Charlton <mike@Wrook>
2010-10-08 14:13:58 +0900

    Reverting accidental editing of this file.

--- lib/jldrill/views/ItemHintView.rb
+++ lib/jldrill/views/ItemHintView.rb
@@ -1,18 +1,17 @@
-require 'jldrill/contexts/DisplayProblemContext'
+require 'Context/View'
 
-module JLDrill::Test
+module JLDrill
     # The ItemHintView displays information about the current item
     # that acts as hints for the user.  For instance, it might
     # indicate that the word is intrasitive, or a suru noun, etc.
-	class ItemHintView < JLDrill::DisplayProblemContext::ProblemView::ItemHintView
+	class ItemHintView < Context::View
 	
 		def initialize(context)
 			super(context)
-            @newProblem = true
 		end
 
 		def newProblem(problem)
-            @newProblem = true
+		    # Should be overridden in the concrete class
 		end	
 
         def updateProblem(problem)
