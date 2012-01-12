78e42c3661044f7ebe55ce24085ce1c9c308ede9
Mike Charlton <mikekchar@gmail.com>
2012-01-12 11:26:58 +0900

    Finish tests for LoadPath

--- spec/jldrill/model/LoadPath_spec.rb
+++ spec/jldrill/model/LoadPath_spec.rb
@@ -26,6 +26,20 @@ module JLDrill
             lp.add(firstDir)
             lp.find("bogus").should eql(nil)
         end
+
+        it "should prioritize directories added first" do
+            lp = LoadPath.new
+            firstDir = File.join(Config::DATA_DIR, "tests/first")
+            secondDir = File.join(Config::DATA_DIR, "tests/second")
+            neverDir = File.join(Config::DATA_DIR, "tests/Never")
+            lp.add(firstDir)
+            lp.add(secondDir)
+            lp.add(neverDir)
+            lp.to_s.should eql("#{firstDir}:#{secondDir}:#{neverDir}")
+            lp.find("bogus").should eql(nil)
+            lp.find("file").should eql(File.join(firstDir, "file"))
+            lp.find("file2").should eql(File.join(secondDir, "file2"))
+        end
     end
 end
 	
