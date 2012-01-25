323327e33084c14cd2501172bd7b4ed6f3e7c6ed
Mike Charlton <mikekchar@gmail.com>
2012-01-25 16:24:22 +0900

    Prepare for 0.6.0 release Also remove mentions of sakabatou. I'm phasing out hosting JLDrill on sakabatou since I'm not sure if I will be keeping the server

--- Rakefile.rb
+++ Rakefile.rb
@@ -37,7 +37,7 @@ pkg_files = FileList[
 release_dir = "jldrill-#{JLDrill::VERSION}"
 
 # Revision of the gem file.  Increment this every time you release a gem file.
-gem_revision ="4"
+gem_revision ="1"
 
 # Rubyforge details
 rubyforge_project = "jldrill"
