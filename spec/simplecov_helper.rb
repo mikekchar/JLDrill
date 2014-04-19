9694c47b4f13ef02e60ed0310d5f5c5940f1909b
Mike Charlton <mikekchar@gmail.com>
2014-04-19 22:24:14 +0100

    Create some simplecov groups

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -1,4 +1,11 @@
 require 'simplecov'
 
 SimpleCov.start do
+  add_filter '/spec/'
+
+  add_group 'Context', 'lib/Context'
+  add_group 'Models', 'lib/jldrill/model'
+  add_group 'Contexts', 'lib/jldrill/contexts'
+  add_group 'Old UI', 'lib/jldrill/oldUI'
+  add_group 'Views', 'lib/jldrill/views'
 end if ENV['COVERAGE']
